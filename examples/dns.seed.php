bafb60d1b851a4201facbd31bc201b11d9c57c4f
Thomas Kerin <thomas.kerin@bitmaintech.com>
2018-10-21 14:00:13 +0200

    dns seed example: allow passing a command line argument specifying the dns seed

--- examples/dns.seed.php
+++ examples/dns.seed.php
@@ -6,6 +6,11 @@ require_once __DIR__ . "/../vendor/autoload.php";
 
 $loop = React\EventLoop\Factory::create();
 
+$seed = 'dnsseed.bitcoin.dashjr.org';
+if ($argc > 1) {
+    $seed = $argv[1];
+}
+
 $factory = new \BitWasp\Bitcoin\Networking\Factory($loop);
 $factory
     ->getDns()
